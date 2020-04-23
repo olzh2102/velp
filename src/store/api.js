71bcab03c5b22a1ece46a53dc0b1a0c4a301c42a
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-23 19:17:51 +0200

    added api actions and action creators

---
+++ src/store/api.js
@@ -0,0 +1,12 @@
+// actions and action creators
+import { createAction } from '@reduxjs/toolkit'
+
+export const apiCallBegan = createAction(
+	'api/CallBegan'
+)
+export const apiCallSuccess = createAction(
+	'api/CallSuccess'
+)
+export const apiCallFailed = createAction(
+	'api/CallFailed'
+)
