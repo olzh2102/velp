2fcc1331b3206cee000af77754791ab12c817152
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-24 11:02:05 +0200

    tried filtering by type and render filtered list in places page component

--- src/pages/places.jsx
+++ src/pages/places.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 
@@ -16,12 +16,30 @@ const PlacesWrapper = styled.div`
 	grid-gap: 1rem;
 `
 
-const PlacesPage = ({ places, getType }) => {
-	const p = getType('burgers')
-	console.log('TYPE:', p)
+const PlacesPage = ({ p, getType }) => {
+	const [places, setPlaces] = useState([])
+	// const p = getType('burgers')
+
+	// console.log('TYPE:', p)
+
+	useEffect(() => setPlaces(p), [p])
+
+	const handleClick = (type) => {
+		const places = getType(type)
+		setPlaces(places)
+		console.log('YOY', places)
+	}
+
 	return (
 		<>
 			<h1>Places Page</h1>
+			<button
+				onClick={() =>
+					handleClick('sushi')
+				}
+			>
+				Sushi
+			</button>
 			<PlacesWrapper>
 				{places.map((p) => (
 					<h1>{p.name}</h1>
@@ -32,7 +50,7 @@ const PlacesPage = ({ places, getType }) => {
 }
 
 const mapStateToProps = (state) => ({
-	places: getTransformedPlaces(state),
+	p: getTransformedPlaces(state),
 	getType: (type) =>
 		getSelectedPlaces(type)(state),
 })
