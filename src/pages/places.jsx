08b0ccef20246978146055c52a31b5dc2e2512a4
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-24 10:39:16 +0200

    testing rendering names of places

--- src/pages/places.jsx
+++ src/pages/places.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 
 import {
 	getTransformedPlaces,
 	getSelectedPlaces,
 } from '../store/places'
 
+const PlacesWrapper = styled.div`
+	display: grid;
+	grid-template-columns: repeat(
+		auto-fill,
+		minmax(450px, 1fr)
+	);
+	grid-gap: 1rem;
+`
+
 const PlacesPage = ({ places, getType }) => {
-	const p = getType('pizza')
+	const p = getType('burgers')
 	console.log('TYPE:', p)
-	return <h1>Places Page</h1>
+	return (
+		<>
+			<h1>Places Page</h1>
+			<PlacesWrapper>
+				{places.map((p) => (
+					<h1>{p.name}</h1>
+				))}
+			</PlacesWrapper>
+		</>
+	)
 }
 
 const mapStateToProps = (state) => ({
